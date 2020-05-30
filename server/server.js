90d7f00235140049458a431b24274de1e316c9b1
noomdalv <vladlunasan@gmail.com>
2020-05-29 23:26:23 -0500

    added mongo config and URI

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://vlad:<password>@cluster0-xp37f.mongodb.net/test?retryWrites=true&w=majority';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
