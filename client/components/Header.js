2d55fd1160e4ee879de64f199ba05c20e7a67ddb
noomdalv <vladlunasan@gmail.com>
2020-05-31 02:09:11 -0500

    created Header component and links

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,13 +1,44 @@
 import React from 'react';
+import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
+import query from '../queries/CurrentUser';
 
 class Header extends React.Component {
+	renderButtons() {
+		const { loading, user } = this.props.data;
+		if (loading) { return <div />};
+
+		if (user) {
+			return <div>Logout</div>;
+		} else {
+			return (
+				<div>
+					<li>
+						<Link to="/signup">SignUp</Link>
+					</li>
+					<li>
+						<Link to="/login">LogIn</Link>
+					</li>
+				</div>
+			)
+		}
+	}
+
 	render() {
+		console.log(this.props.data)
 		return (
-			<div>
-				Header
-			</div>
+			<nav>
+				<div className="nav-wrapper">
+					<Link to="/" className="brand-logo left">
+						Home
+					</Link>
+					<ul className="right">
+						{ this.renderButtons() }
+					</ul>
+				</div>
+			</nav>
 		)
 	}
 };
 
-export default Header;
+export default graphql(query)(Header);
